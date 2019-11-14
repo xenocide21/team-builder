fbb6d7f92a3be27a9990a4d4d77de558c1747580
Ashley Dunham <xenocide21@gmail.com>
2019-11-14 09:42:49 -0700

    simplified project to meet MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import './App.css';
-import TeamNames from "./components/TeamNames";
-import Name from "./components/Name"
-import { Route } from "react-router-dom"
-import FormComponent from "./components/FormComponent";
+import React, { useState } from 'react';
 
-function App() {
+import Form from './Form';
+
+import './App.css';
+
+const App = () => {
+  const [id, setId] = useState(2);
+  const [members, setMembers] = useState([
+    { id: '1', name: "ashley", email: "ashley@test.com", role: "code wizard" }
+  ]);
+
+  console.log("Members: ", members);
 
   return (
-      <div id="main">
-        <h1>Sword Art Online</h1>
-        <FormComponent />
-        <Route exact path="/" component={TeamNames}/>
-        <Route path="/names/:id" render={props => (<Name {...props}/>)}/>
+    <>
+      <Form members={members} id={id} setMembers={setMembers} setId={setId} />
+      <br />
 
-      </div>
-  );
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+            members.map((member) => {
+              return (
+                <tr key={member.id}>
+                  <td>{member.id}</td>
+                  <td>{member.name}</td>
+                  <td>{member.email}</td>
+                  <td>{member.role}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </>
+  )
 }
 
 export default App;
